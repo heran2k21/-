040e962c5f74d986fa59313babf522580c730735
heran2k21 <112861404+heran2k21@users.noreply.github.com>
2025-09-13 05:01:23 +0800

    收集所有宠物.lua

--- 收集所有宠物.lua
+++ 收集所有宠物.lua
@@ -1,4 +1,4 @@
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/heran2k21/-/refs/heads/main/heran01227"))()
 
 local X = Material.Load({
     Title = "收集所有宠物丨By:Heran",
